3ad43e8c9adb23e56f5b162604f16f7597627e0d
Dat Nguyen <datnguyen07082003@gmail.com>
2023-11-25 14:32:45 +0700

    Dat

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
+     include "model/pdo.php";
+     include "model/danhmuc.php";
+     include "model/phong.php";
+     include "model/taikhoan.php";
+     
+
+
+
 include "view/header.php";
 if(isset($_GET['pg'])&&($_GET['pg']!="")){
     $pg=$_GET['pg'];
     switch($pg){
-        case "":
-           
-            break;}
+        
+            break;
         }
+    }
 else{
     include "view/home.php";
 }
